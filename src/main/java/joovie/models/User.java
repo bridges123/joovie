9a058e35bd0b1973e1e8d818348745b6dddaca41
bridges123 <i.s.andreevmail.rut@gmail.com>
2022-11-03 23:12:49 +0300

    follow realization and view count in profile

--- src/main/java/joovie/models/User.java
+++ src/main/java/joovie/models/User.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import java.util.List;
+import java.util.Set;
 
 
 @Entity
@@ -39,6 +40,14 @@ public class User {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
     private List<Video> videos;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="followers_rel",
+            joinColumns={@JoinColumn(name="user_id")},
+            inverseJoinColumns={@JoinColumn(name="follower_id")})
+    private Set<User> followers;
+
+    @ManyToMany(mappedBy = "followers")
+    private Set<User> following;
 
     public User(String UID, String email, String password, String username, String avatar, Role role, Status status) {
         this.UID = UID;
